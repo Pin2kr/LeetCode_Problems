40ae7aa200f92bd63400823efddc98a3f6df1d6a
Pin2kr <kumarpintumahto10@gmail.com>
2024-09-23 11:14:48 +0530

    Time: 5 ms (70.36%), Space: 56.8 MB (96.72%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+        //using two pointer
+
+        int lp=0,rp=(height.length-1),max=0;
+
+        while(lp<rp){
+            //water contain-width*height
+
+            int w=rp-lp;
+            int h=Math.min(height[lp],height[rp]);
+            int curW=w*h;
+            max=Math.max(max,curW);
+
+       if(height[lp]<height[rp]){
+        lp++;
+       }else{
+        rp--;
+       }
+
+        } 
+         return max; 
+    }
+}
